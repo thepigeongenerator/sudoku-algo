a55da88ec2235a8f555a35a38d85db07fcc3a250
Quinn <quinnthepigeon@proton.me>
2025-07-28 08:55:21 +0200

    write header for integer definitions

---
+++ src/util/intdef.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdint.h>
+
+/* variable-width integer types that are commonly used */
+typedef long long llong;           // ≥64 bit (signed) integer
+typedef unsigned int uint;         // ≥16 bit unsigned integer
+typedef unsigned long ulong;       // ≥32 bit unsigned integer
+typedef unsigned long long ullong; // ≥64 bit unsigned integer
+
+/* fixed-width integer types */
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
